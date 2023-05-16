194d3b6d9fcece47723afa4e063e7c02655cd36c
Csalex01 <csillag.alex@student.ms.sapientia.ro>
2023-05-16 14:55:32 +0300

    Fixed image padding issue

--- Create_Training_Set.m
+++ Create_Training_Set.m
@@ -5,9 +5,6 @@ close all;
 disp("Tanítóhalmaz előkészítése folyamatban...");
 
 Crop_Images;
-
-return;
-
 Resize_Images;
 
 disp("Kész!");
--- pad_image.m
+++ pad_image.m
@@ -1,8 +1,19 @@
 function new_img = pad_image(new_width, new_height, img)
     img_size = size(img);
 
-    height_diff = floor(new_height - img_size(1));
-    width_diff = floor(new_width - img_size(2));
+    if mod(new_height, 2) == 1
+        new_height = new_height + 1;
+    end
+
+    if mod(new_width, 2) == 1
+        new_width = new_width + 1;
+    end
+
+    height_diff = floor((new_height - img_size(1)) / 2);
+    width_diff = floor((new_width - img_size(2)) / 2);
     
     new_img = padarray(img, [ height_diff width_diff ]);
+
+    new_img = imresize(new_img, [ new_height new_width ]);
+
 end
