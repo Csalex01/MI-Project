0e68fed1f43375cf4a2d5caf976132acb6f3bd1b
Csalex01 <csillag.alex@student.ms.sapientia.ro>
2023-05-30 18:03:08 +0300

    Final Project

--- pad_image.m
+++ pad_image.m
@@ -1,19 +1,25 @@
 function new_img = pad_image(new_width, new_height, img)
+
+    % Kép méretének meghatározása
     img_size = size(img);
 
-    if mod(new_height, 2) == 1
-        new_height = new_height + 1;
+    % Kép -> Négyzet
+    if new_width > new_height
+        
+        new_height = new_width;
+    
+    elseif new_height > new_width
+    
+        new_width = new_height;
+        
     end
 
-    if mod(new_width, 2) == 1
-        new_width = new_width + 1;
-    end
-
+    % Szélesség és magasság különbségeinek meghatározása
     height_diff = floor((new_height - img_size(1)) / 2);
     width_diff = floor((new_width - img_size(2)) / 2);
     
+    % Kép párnázása illetve méretezése
     new_img = padarray(img, [ height_diff width_diff ]);
-
     new_img = imresize(new_img, [ new_height new_width ]);
 
 end
